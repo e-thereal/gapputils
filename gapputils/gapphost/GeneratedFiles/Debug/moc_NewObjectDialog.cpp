1f356abf1c5f73b7b4d05235e3da24581a015ea5
brosch.tom <brosch.tom@gmail.com>
2011-06-02 07:15:36 +0000

    Started to make the project vs2010 ready again.

--- gapputils/gapphost/GeneratedFiles/Debug/moc_NewObjectDialog.cpp
+++ gapputils/gapphost/GeneratedFiles/Debug/moc_NewObjectDialog.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'NewObjectDialog.h'
 **
-** Created: Mon May 30 16:59:37 2011
+** Created: Thu Jun 2 00:05:08 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
