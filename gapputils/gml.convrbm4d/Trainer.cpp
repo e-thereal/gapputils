8d934b23b4f92eac6771c1bcde7c49ca5a51c973
tombr <tombr@msmri.medicine.ubc.ca>
2015-07-10 11:29:20 -0700

    Added categories to Trainer.cpp

--- gapputils/gml.convrbm4d/Trainer.cpp
+++ gapputils/gml.convrbm4d/Trainer.cpp
@@ -7,6 +7,8 @@
 
 #include "Trainer.h"
 
+#include <gapputils/attributes/GroupAttribute.h>
+
 namespace gml {
 
 namespace convrbm4d {
@@ -15,46 +17,47 @@ BeginPropertyDefinitions(Trainer, Description("Trains a convolutional RBM using 
 
   ReflectableBase(DefaultWorkflowElement<Trainer>)
 
-  WorkflowProperty(InitialModel, Input("CRBM"), NotNull<Type>(), Description("Required initial model. The initial model can be a previously trained model, are a new model initialized using Initialize"))
-  WorkflowProperty(Tensors, Input("Ts"), NotNull<Type>(), NotEmpty<Type>(), Description("The training set."))
   WorkflowProperty(DbmLayer, Enumerator<Type>())
 
-  WorkflowProperty(EpochCount, Description("Number of sweeps through the entire training set."))
-  WorkflowProperty(TrialEpochCount)
-  WorkflowProperty(BatchSize, Description("Number of images used per gradient update."))
-  WorkflowProperty(FilterBatchSize, Description("Number of filters that are processed in parallel."))
-  WorkflowProperty(SubRegionCount, Description("Number of sub-regions into which the calculation will be split. Fewer (but larger) sub-regions speed up the calculation but require more memory."))
-  WorkflowProperty(GpuCount, Description("Number of GPUs used for training."))
+  WorkflowProperty(EpochCount, Group("Optimization"), Description("Number of sweeps through the entire training set."))
+  WorkflowProperty(TrialEpochCount, Group("Optimization"))
+  WorkflowProperty(BatchSize, Group("Optimization"), Description("Number of images used per gradient update."))
 
-  WorkflowProperty(SparsityMethod, Enumerator<Type>())
-  WorkflowProperty(SparsityTarget, Description("Target expected activation of a hidden unit."))
-  WorkflowProperty(SparsityWeight, Description("Weight of the sparsity target relative to the learning rate."))
+  WorkflowProperty(FilterBatchSize, Group("Performance"), Description("Number of filters that are processed in parallel."))
+  WorkflowProperty(SubRegionCount, Group("Performance"), Description("Number of sub-regions into which the calculation will be split. Fewer (but larger) sub-regions speed up the calculation but require more memory."))
+  WorkflowProperty(GpuCount, Group("Performance"), Description("Number of GPUs used for training."))
 
-  WorkflowProperty(CdIterations, Description("Number of CD iterations. (1: CD learning, >1: CD-n learning)"))
-  WorkflowProperty(Method, Enumerator<Type>())
-  WorkflowProperty(LearningRates, NotEmpty<Type>(), Description("Initial values of the decaying learning rates of the filters."))
-  WorkflowProperty(LearningDecay, Description("After how many epochs the learning rate will be halved. A value of -1 indicates no LearningDecay."))
-  WorkflowProperty(InitialMomentum, Description("Momentum used for the first epoch."))
-  WorkflowProperty(FinalMomentum)
-  WorkflowProperty(MomentumDecayEpochs)
-  WorkflowProperty(WeightDecay)
-  WorkflowProperty(InitialWeights, Description("If given, these weights will be tested as initial weights and will override the initial weights."))
-  WorkflowProperty(SignalToNoiseRatio, Description("If greater than 0, the standard deviation of the noise of the initial filters is set to 'sd(learned filters) / SNR'."))
-  WorkflowProperty(RandomizeTraining, Flag(), Description("Randomly select images of a mini-batch."))
+  WorkflowProperty(SparsityMethod, Group("Optimization"), Enumerator<Type>())
+  WorkflowProperty(SparsityTarget, Group("Optimization"), Description("Target expected activation of a hidden unit."))
+  WorkflowProperty(SparsityWeight, Group("Optimization"), Description("Weight of the sparsity target relative to the learning rate."))
+
+  WorkflowProperty(CdIterations, Group("Optimization"), Description("Number of CD iterations. (1: CD learning, >1: CD-n learning)"))
+  WorkflowProperty(Method, Group("Optimization"), Enumerator<Type>())
+  WorkflowProperty(LearningRates, Group("Optimization"), NotEmpty<Type>(), Description("Initial values of the decaying learning rates of the filters."))
+  WorkflowProperty(LearningDecay, Group("Optimization"), Description("After how many epochs the learning rate will be halved. A value of -1 indicates no LearningDecay."))
+  WorkflowProperty(InitialMomentum, Group("Optimization"), Description("Momentum used for the first epoch."))
+  WorkflowProperty(FinalMomentum, Group("Optimization"))
+  WorkflowProperty(MomentumDecayEpochs, Group("Optimization"))
+  WorkflowProperty(WeightDecay, Group("Optimization"))
+  WorkflowProperty(InitialWeights, Group("Optimization"), Description("If given, these weights will be tested as initial weights and will override the initial weights."))
+  WorkflowProperty(SignalToNoiseRatio, Group("Optimization"), Description("If greater than 0, the standard deviation of the noise of the initial filters is set to 'sd(learned filters) / SNR'."))
+  WorkflowProperty(RandomizeTraining, Group("Optimization"), Flag(), Description("Randomly select images of a mini-batch."))
+
   WorkflowProperty(ShareBiasTerms, Flag(), Description("If checked, visible and hidden units of the same filter share bias terms."))
   WorkflowProperty(ChannelsPerBlock, Description("All channels of the same pooling block share the same bias terms when shared bias terms are active. Hence, this number must be known."))
-  WorkflowProperty(DropoutMethod, Enumerator<Type>(), Description("Defines if entire columns or individual hidden units are dropped."))
-  WorkflowProperty(VisibleDropout, Description("Probability of a visible unit of being ignored. (currently not used)"))
-  WorkflowProperty(HiddenDropout, Description("Probability of a hidden unit of being ignored."))
-  WorkflowProperty(FilterDropout, Description("Probability of an entire batch of filters being ignored. To drop individual filters, set the filter batch size to 1"))
+  WorkflowProperty(DropoutMethod, Group("Optimization"), Enumerator<Type>(), Description("Defines if entire columns or individual hidden units are dropped."))
+  WorkflowProperty(VisibleDropout, Group("Optimization"), Description("Probability of a visible unit of being ignored. (currently not used)"))
+  WorkflowProperty(HiddenDropout, Group("Optimization"), Description("Probability of a hidden unit of being ignored."))
+  WorkflowProperty(FilterDropout, Group("Optimization"), Description("Probability of an entire batch of filters being ignored. To drop individual filters, set the filter batch size to 1"))
   WorkflowProperty(CalculateError, Flag(), Description("If checked, the reconstruction error is calculated"))
   WorkflowProperty(UpdateModel, Description("If greater than 0, the model is updated every <UpdateModel> epochs."))
 
-
-  WorkflowProperty(CurrentEpoch, NoParameter())
-  WorkflowProperty(Model, Output("CRBM"), Description("The trained model."))
-  WorkflowProperty(AverageEpochTime, Output("T"))
-  WorkflowProperty(ReconstructionError, NoParameter())
+  WorkflowProperty(InitialModel, Group("Input/output"), Input("CRBM"), NotNull<Type>(), Description("Required initial model. The initial model can be a previously trained model, are a new model initialized using Initialize"))
+  WorkflowProperty(Tensors, Group("Input/output"), Input("Ts"), NotNull<Type>(), NotEmpty<Type>(), Description("The training set."))
+  WorkflowProperty(CurrentEpoch, Group("Input/output"), NoParameter())
+  WorkflowProperty(Model, Group("Input/output"), Output("CRBM"), Description("The trained model."))
+  WorkflowProperty(AverageEpochTime, Group("Input/output"), Output("T"))
+  WorkflowProperty(ReconstructionError, Group("Input/output"), NoParameter())
 
 EndPropertyDefinitions
 
